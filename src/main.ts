96eeaaa9a9356f2ec4225267b8738920430d9c2b
sacopon <sacopon.dev@gmail.com>
2024-01-13 16:20:38 +0900

    add entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("hello");
+class Proj2024 {
+	/**
+	 * エントリーポイント
+	 */
+	public static run() {
+		const main = new Proj2024();
+		main.execute();
+	}
+
+	public execute(): void {
+		console.log("Proj2024#execute()");
+	}
+}
+
+window.addEventListener("DOMContentLoaded", Proj2024.run);
