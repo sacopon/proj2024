eb22b73b378acd7e88df4cb6152bc9de83b87495
sacopon <sacopon.dev@gmail.com>
2024-01-14 13:14:44 +0900

    シーンのルートコンテナ作成

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Application, Sprite, Assets, Container } from "pixi.js";
 import init, { greet } from "../wasm/pkg";
 import { Screen } from "./application/utilities/screen";
 import { disableTouchEvent, disableOuterCanvasTouchEvent } from "./application/utilities/disable_touch_event";
+import { SceneRootContainer } from "./presentation/core/scene_root_container";
 
 class Proj2024 {
 	private static m_instance: Proj2024 | null = null;
@@ -10,7 +11,7 @@ class Proj2024 {
 	private m_state = 0;
 	private m_sprite: Sprite | null = null;
 	private s2: Sprite[] = [];
-	private m_sceneRoot: Container;
+	private m_sceneRoot: SceneRootContainer;
 
 	/**
 	 * エントリーポイント
@@ -52,14 +53,7 @@ class Proj2024 {
 		if (this.m_state === 0) {
 			++this.m_state;
 
-			// シーンのルートオブジェクトをセーフエリアの位置に配置
-			// 原点がセーフエリア左上となる
-			// TODO: ↑混乱するので、SceneRootContainer をクラスとして作成し、
-			// 原点、サイズ、画面左上(セーフエリア外)、画面サイズ(セーフエリアより大きい)を取得できるようにまとめる
-			this.m_sceneRoot = new Container();
-			this.m_app.stage.addChild(this.m_sceneRoot);
-			this.m_sceneRoot.x = this.m_screen.safeArea.x;
-			this.m_sceneRoot.y = this.m_screen.safeArea.y;
+			this.m_sceneRoot = new SceneRootContainer(this.m_screen, this.m_app.stage);
 
 			Assets.addBundle("resources", [
 				{alias: "neko", src: "/images/neko.jpg"},
@@ -70,8 +64,8 @@ class Proj2024 {
 			this.m_sceneRoot.addChild(this.m_sprite);
 
 			const sprite = this.m_sprite!;
-			sprite.x = Math.floor(this.m_screen.safeArea.width  / 2 - sprite.width  / 2);
-			sprite.y = Math.floor(this.m_screen.safeArea.height / 2 - sprite.height / 2);
+			sprite.x = this.m_sceneRoot.center.x - Math.floor(sprite.width  / 2);
+			sprite.y = this.m_sceneRoot.center.y - Math.floor(sprite.height / 2);
 
 			for (let i = 0; i < 4; ++i) {
 				this.s2.push(Sprite.from(Assets.get("s")));
@@ -85,9 +79,8 @@ class Proj2024 {
 	public onResize() {
 		this.m_screen = new Screen(window.innerWidth, window.innerHeight);
 		this.m_app.renderer.resize(this.m_screen.size.width, this.m_screen.size.height);
-
-		this.m_sceneRoot.x = this.m_screen.safeArea.x;
-		this.m_sceneRoot.y = this.m_screen.safeArea.y;
+		this.m_sceneRoot.onResize(this.m_screen);
+		this.setSpritePos();
 	}
 
 	public setSpritePos() {
@@ -99,23 +92,31 @@ class Proj2024 {
 
 		// 左上
 		s = this.s2[0];
-		s.x = 0;
-		s.y = 0;
+		// s.x = 0;
+		// s.y = 0;
+		s.x = this.m_sceneRoot.screen.x;
+		s.y = this.m_sceneRoot.screen.y;
 
 		// 右上
 		s = this.s2[1];
-		s.x = this.m_screen.safeArea.width - s.width;
-		s.y = 0;
+		// s.x = this.m_screen.safeArea.width - s.width;
+		// s.y = 0;
+		s.x = this.m_sceneRoot.screen.x + this.m_sceneRoot.screen.width - s.width;
+		s.y = this.m_sceneRoot.screen.y;
 
 		// 左下
 		s = this.s2[2];
-		s.x = 0;
-		s.y = this.m_screen.safeArea.height - s.height;
+		// s.x = 0;
+		// s.y = this.m_screen.safeArea.height - s.height;
+		s.x = this.m_sceneRoot.screen.x;
+		s.y = this.m_sceneRoot.screen.y + this.m_sceneRoot.screen.height - s.height;
 
 		// 右下
 		s = this.s2[3];
-		s.x = this.m_screen.safeArea.width  - s.width;
-		s.y = this.m_screen.safeArea.height - s.height;
+		// s.x = this.m_screen.safeArea.width  - s.width;
+		// s.y = this.m_screen.safeArea.height - s.height;
+		s.x = this.m_sceneRoot.screen.x + this.m_sceneRoot.screen.width  - s.width;
+		s.y = this.m_sceneRoot.screen.y + this.m_sceneRoot.screen.height - s.height;
 	}
 }
 
